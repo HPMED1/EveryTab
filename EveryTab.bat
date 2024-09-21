7c01f14dcc62c9b43f22ce51ea8717fa2ada830a
HPMED <62830536+HPMED1@users.noreply.github.com>
2024-09-21 19:44:18 +0300

    1.1
    fixed some errors

--- EveryTab.bat
+++ EveryTab.bat
@@ -107,7 +107,7 @@ ping localhost -n 1 >nul
 echo             [90;1m╚═╦═══»[0m  [92m[Back][0m      [95m[5][0m
 ping localhost -n 1 >nul
 echo|set /p=".              [90;1m╚══>[0m"
-choice /c 1234 >nul
+choice /c 12345 >nul
 if /I "%errorlevel%" EQU "1" (
   start "" https://everythingmoe.com
   goto :Start
@@ -167,7 +167,7 @@ ping localhost -n 1 >nul
 echo         [90;1m╚═╦═════»[0m  [92m[Back][0m  [95m[3][0m
 ping localhost -n 1 >nul
 echo|set /p=".          [90;1m╚══>[0m"
-choice /c 1234 >nul
+choice /c 123 >nul
 if /I "%errorlevel%" EQU "1" (
   start "" https://www.youtube.com
   goto :Start
@@ -416,7 +416,7 @@ echo         [90;1m╚═╦═════»[0m    [92m[Piracy][0m    [95m
 ping localhost -n 1 >nul
 
 echo|set /p=".          [90;1m╚══>[0m"
-choice /c 1234 >nul
+choice /c 123 >nul
 if /I "%errorlevel%" EQU "1" (
   goto :Entertainment
 )
